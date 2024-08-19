72b6675f55b7ce8671a0831f87d8413392168918
pranav <106530024+pranavc1515@users.noreply.github.com>
2024-08-19 17:57:54 +0530

    changes created

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
+const cors = require('cors'); // Import CORS
 const app = express();
 const port = process.env.PORT || 4000;
-const cors = require('cors');
-app.use(cors());
+
+app.use(cors()); // Enable CORS for all routes
 
 app.get('/', (req, res) => {
-    res.send('Hello, how are you?');
+  res.send('Hello, how are you?');
 });
 
 app.get('/greet', (req, res) => {
-    res.send('I am fine from the backend.');
+  res.send('I am fine from the backend.');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
